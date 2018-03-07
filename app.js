30d67455cfb4a04d8614d5d0139f83341dd3d1b6
Edward Hart <hart.edward91@gmail.com>
2018-03-07 09:46:12 -0500

    first update to Jackson

---
+++ app.js
@@ -0,0 +1,86 @@
+
+// My Name
+let name = "Edward Hart";
+
+// Number of States
+const numOfStates = 50;
+
+// Sum of Addition
+let sum = 5 + 4;
+
+// Back of line
+let firstLetter = prompt("What is your name?");
+
+if (firstLetter.charCodeAt(0) <= 76){
+    alert("Next!");
+}   else {
+    alert("Back of the line!");
+};
+
+
+// Hello World Function
+function sayHello() {
+    alert("Hello World!");
+}
+
+sayHello();
+
+// Check Age Function
+function checkAge(name, age) {
+    if(age < 21){
+        alert("Sorry " + name + " you are not old enough to view this page!");
+    }
+}
+
+checkAge("Charles", 21);
+checkAge("Abby", 27);
+checkAge("James", 18);
+checkAge("John", 17);
+
+// Favorite Veggies
+let veggies = ["Carrots", "Celery", "Broccoli"];
+
+for(i = 0; i < veggies.length; i++){
+    console.log(veggies[i]);
+};
+
+// Array of Strangers
+let strangers = [
+    {
+        name: "Teddy Bear",
+        age: 40
+    },
+    {
+        name: "Willie Taggert",
+        age: 23
+    },
+    {
+        name: "Booby Miles",
+        age: 18
+    },
+    {
+        name: "Theresa Adams",
+        age: 20
+    },
+    {
+        name: "Chester Danger",
+        age: 22
+    },
+];
+
+// The checkAge for loop
+
+
+
+
+// The getLengthFunction
+function getLength(word){
+    let word = ""
+    for(let char of word){
+        
+    }
+}
+
+
+
+
